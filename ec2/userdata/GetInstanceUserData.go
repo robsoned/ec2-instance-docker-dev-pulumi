b8f51036af921085b9e6b8b38be87f911d958f66
robsoned <robsoneduardosantos@gmail.com>
2025-03-26 20:13:13 +0000

    Enhance EBS volume handling in userdata script: add checks for volume availability, filesystem validation, and mount point management

--- ec2/userdata/GetInstanceUserData.go
+++ ec2/userdata/GetInstanceUserData.go
@@ -15,19 +15,67 @@ func GetInstanceUserData(ctx *pulumi.Context) pulumi.String {
 	
 set -e
 
-# Add github cli repo (gh) and install docker, git and gh
-type -p yum-config-manager >/dev/null || sudo yum install yum-utils
-sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+HOME=/home/ec2-user
+EBS_VOLUME_DEVICE_NAME=/dev/sdh
+EBS_VOLUME_FOLDER=ebs-volume
+MAX_RETRIES=10
+RETRY_INTERVAL=10
 
-sudo yum install docker-%s git gh -y && \
-sudo usermod -a -G docker ec2-user && \
-sudo curl -L "https://github.com/docker/compose/releases/download/%s/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-sudo chmod +x /usr/local/bin/docker-compose && \
-sudo service docker start && \
+# Wait for the EBS volume to exist
+for i in $(seq 1 $MAX_RETRIES); do
+  if [ -b $EBS_VOLUME_DEVICE_NAME ]; then
+    echo "EBS volume $EBS_VOLUME_DEVICE_NAME is now available."
+    break
+  fi
+
+  echo "Waiting for EBS volume $EBS_VOLUME_DEVICE_NAME to become available... (attempt $i/$MAX_RETRIES)"
+  sleep $RETRY_INTERVAL
+done
+
+# Check if the volume is already mounted
+if grep -qs $EBS_VOLUME_DEVICE_NAME /proc/mounts; then
+  echo "Volume already mounted"
+  exit 0
+fi
+
+# Check if the filesystem already exists
+if ! blkid $EBS_VOLUME_DEVICE_NAME; then
+  mkfs -t ext4 $EBS_VOLUME_DEVICE_NAME
+else
+  # Check if the filesystem is valid
+  if ! fsck -n $EBS_VOLUME_DEVICE_NAME; then
+    echo "Invalid filesystem detected. Creating a new filesystem..."
+    mkfs -t ext4 $EBS_VOLUME_DEVICE_NAME
+  fi
+fi
+
+mkdir -p $HOME/$EBS_VOLUME_FOLDER
+
+# Check if the mount point is already in use
+if mountpoint -q $HOME/$EBS_VOLUME_FOLDER; then
+  echo "Mount point already in use"
+  exit 0
+elif ! mount $EBS_VOLUME_DEVICE_NAME $HOME/$EBS_VOLUME_FOLDER; then
+  echo "Failed to mount the volume. Checking filesystem..."
+  fsck -y $EBS_VOLUME_DEVICE_NAME
+  mount $EBS_VOLUME_DEVICE_NAME $HOME/$EBS_VOLUME_FOLDER
+fi
+
+chown -R ec2-user:ec2-user $HOME/$EBS_VOLUME_FOLDER
+
+# Add github cli repo (gh) and install docker, git and gh
+type -p yum-config-manager >/dev/null || yum install yum-utils
+yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+
+yum install docker-%s git gh -y && \
+usermod -a -G docker ec2-user && \
+curl -L "https://github.com/docker/compose/releases/download/%s/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chmod +x /usr/local/bin/docker-compose && \
+service docker start && \
 # login to docker registry
 su ec2-user -c 'echo "%s" | docker login %s -u %s --password-stdin' && \
-sudo service docker restart && \
-echo "ClientAliveInterval 60" | sudo tee -a /etc/ssh/sshd_config && \
+service docker restart && \
+echo "ClientAliveInterval 60" | tee -a /etc/ssh/sshd_config && \
 systemctl restart sshd
 `, getDockerVersion(ctx), getDockerComposeVersion(ctx), registryConfig.Password, registryConfig.Server, registryConfig.Username)
 
